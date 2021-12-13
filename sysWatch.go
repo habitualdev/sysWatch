59f6ffb575ef8cd8f09d6448f68811d81ef40f7a
habitual <habitual@habitual-media.com>
2021-12-13 06:43:21 -0500

    first commit

---
+++ sysWatch.go
@@ -0,0 +1,109 @@
+package main
+
+import(
+	g "github.com/AllenDang/giu"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var wnd = g.NewMasterWindow("sysWatch", 800, 600, 0)
+var tabBarLayout g.Layout
+
+
+var stringsTableEnabled string
+var displayStringEnabled string
+
+var displayStringDisabled string
+var stringsTableDisabled string
+
+var displayStringRunning string
+var stringsTableRunning string
+
+
+
+
+func rootCheck() {
+	cmd := exec.Command("id", "-u")
+	output, err := cmd.Output()
+
+	rootInt, _ := strconv.Atoi(strings.ReplaceAll(string(output),"\n",""))
+
+	if rootInt == 0 {
+		tabBarLayout= g.Layout{
+			g.TabBar().TabItems(g.TabItem("Enabled").Layout(g.Label(displayStringEnabled)),g.TabItem("Disabled").Layout(g.Label(displayStringDisabled)),g.TabItem("Running").Layout(g.Label(displayStringRunning))),
+		}
+	} else {
+		tabBarLayout = g.Layout{
+			g.Label("You must run this application as root"),
+		}
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+func loop() {
+	rootCheck()
+	w1 := g.SingleWindow()
+	w1.Layout(tabBarLayout)
+
+}
+
+func getSysEnabled(){
+	sysGet := exec.Command("systemctl","list-unit-files","--state=enabled")
+	stringsTableEnabledB, _ := sysGet.Output()
+	stringsTableEnabled = string(stringsTableEnabledB)
+
+	lineSplit := strings.Split(stringsTableEnabled,"\n")
+	for _, line := range lineSplit{
+		if len(strings.Fields(line)) == 3 {
+			displayStringEnabled = displayStringEnabled + strings.Fields(line)[0] + "\n"
+		}
+	}
+	time.Sleep(time.Second * 2)
+}
+
+func getSysDisabled(){
+	sysGet := exec.Command("systemctl","list-unit-files","--state=disabled")
+	stringsTableDisabledB, _ := sysGet.Output()
+	stringsTableDisabled = string(stringsTableDisabledB)
+
+	lineSplit := strings.Split(stringsTableDisabled,"\n")
+	for _, line := range lineSplit{
+		if len(strings.Fields(line)) == 3 {
+			displayStringDisabled = displayStringDisabled + strings.Fields(line)[0] + "\n"
+		}
+	}
+	time.Sleep(time.Second * 2)
+}
+
+func getSysRunning(){
+	sysGet := exec.Command("systemctl","list-units","--type=service","--state=active")
+	stringsTableRunningB, _ := sysGet.Output()
+	stringsTableRunning = string(stringsTableRunningB)
+
+	lineSplit := strings.Split(stringsTableRunning,"\n")
+	for _, line := range lineSplit{
+		if len(strings.Fields(line)) >= 5 {
+			if strings.Fields(line)[0] != strings.ToUpper(strings.Fields(line)[0]) {
+
+			displayStringRunning = displayStringRunning + strings.Fields(line)[0] + "\n"
+		}
+		}
+	}
+	time.Sleep(time.Second * 2)
+}
+
+
+func main() {
+	go getSysEnabled()
+	go getSysDisabled()
+	go getSysRunning()
+
+wnd.Run(loop)
+}
